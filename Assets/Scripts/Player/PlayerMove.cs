6e2955f9fea7d7a28bb986d9e03de1a66cc91d67
Albert.lee <albert.l@cheilpengtai.com>
2017-12-21 11:24:54 +0900

    complete chapter 1

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+	public float speed = 0.5f;
+	private Transform playerTf;
+	private Vector3 playerPos;
+
+	private void Awake()
+	{
+		playerTf = transform;
+	}
+
+	private void Start()
+	{
+		playerPos = playerTf.position;
+	}
+
+	private void FixedUpdate()
+	{
+		playerPos.x += speed;
+		playerTf.position = playerPos;
+	}
+}
